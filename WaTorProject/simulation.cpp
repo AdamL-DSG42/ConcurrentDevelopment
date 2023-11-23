4f18acf852005c57f0d456f2267da8440bc3379e
Adam <adam@pop-os.localdomain>
2023-11-23 13:51:28 +0000

    Working sequential version of the Wa-Tor simulation

--- WaTorProject/simulation.cpp
+++ WaTorProject/simulation.cpp
@@ -36,25 +36,36 @@
 #include <vector>
 #include <tuple>
 #include <ctime>
-#include "fish.h"
-#include "shark.h"
+// #include "fish.h"
+// #include "shark.h"
 #include "tileConstants.h"
 
 const int gridSize = xdim*ydim;
 int cellXSize = WindowXSize/xdim;
 int cellYSize = WindowYSize/ydim;
+
+struct Tile {
+  bool alreadyMoved = false;
+  int breedTime = 0;
+  int breedOccur = 0;
+  int starveTime = 0;
+  int starveDeath = 0;
+  int type = water;
+  sf::Color creatureColour = sf::Color::Blue;
+};
+
 sf::RectangleShape watorArray[xdim][ydim];
-GridTile* worldData[xdim][ydim];
+Tile worldData[xdim][ydim];
 
 // Swap the value of two integers
-void swap (int &a, int &b){ 
-    int temp = a; 
+void swap (Tile &a, Tile &b){ 
+    Tile temp = a; 
     a = b; 
     b = temp; 
 } 
 
 // Randomly shuffle all of the tiles in the flattened 2D array
-void shuffle(int *arr, int rows, int cols){
+void shuffle(Tile *arr, int rows, int cols){
   int totalSize = rows * cols;
 
   for(int i = totalSize - 1; i > 0; --i){
@@ -63,71 +74,155 @@ void shuffle(int *arr, int rows, int cols){
   }
 }
 
-// Create a vector of all empty neighbours of a tile
-void getEmptyNeighbours(int row, int col, std::vector<std::pair<int, int>>& emptyNeighbours){
-  if(worldData[row][(col + 1) % ydim] == water){
-    emptyNeighbours.push_back(std::make_pair(row, ((col + 1) % ydim)));
-  }
-  if(worldData[row][(col - 1) % ydim] == water){
-    emptyNeighbours.push_back(std::make_pair(row, ((col - 1) % ydim)));
-  }
-  if(worldData[(row + 1) % xdim][col] == water){
-    emptyNeighbours.push_back(std::make_pair(((row + 1)) % xdim, col));
-  }
-  if(worldData[(row - 1) % xdim][col] == water){
-    emptyNeighbours.push_back(std::make_pair(((row - 1)) % xdim, col));
-  }
-  for (int i = 0; i < emptyNeighbours.size(); i++) {
-    // Printing the element at
-    // index 'i' of vector
-    std::cout << "Empty Neighbour Vector: " << emptyNeighbours[i].first << ", " << emptyNeighbours[i].second << std::endl;
+void moveReset(){
+  for(int i = 0; i < xdim; ++i){
+    for(int k = 0; k < ydim; ++k){
+      worldData[i][k].alreadyMoved = false;
+    }
   }
 }
 
-// Find a random neighbour of a tile that is a fish
-// std::tuple<int, int> getRandomFishNeighbour(int row, int col){
-//   std::vector<int> neighbours = getNeighbours(row, col);
-//   int randRow = 0;
-//   int randCol = 0;
-//   if(std::find(neighbours.begin(), neighbours.end(), fish) != neighbours.end()){
+// Create a vector of all empty neighbours of a tile
+void getFishNeighbours(int row, int col, std::vector<std::pair<int, int>>& fishNeighbours){
+  if(worldData[row][(((col + 1) % ydim) + ydim) % ydim].type == fish){
+    fishNeighbours.push_back(std::make_pair(row, ((((col + 1) % ydim) + ydim) % ydim)));
+  }
+  if(worldData[row][(((col - 1) % ydim) + ydim) % ydim].type == fish){
+    fishNeighbours.push_back(std::make_pair(row, ((((col - 1) % ydim) + ydim) % ydim)));
+  }
+  if(worldData[(((row + 1) % xdim) + xdim) % xdim][col].type == fish){
+    fishNeighbours.push_back(std::make_pair(((((row + 1) % xdim) + xdim) % xdim), col));
+  }
+  if(worldData[(((row - 1) % xdim) + xdim) % xdim][col].type == fish){
+    fishNeighbours.push_back(std::make_pair(((((row - 1) % xdim) + xdim) % xdim), col));
+  }
+}
 
-//     return {randRow, randCol};
-//   }
-//   return {-1, -1};
-// }
+// Create a vector of all empty neighbours of a tile
+void getEmptyNeighbours(int row, int col, std::vector<std::pair<int, int>>& emptyNeighbours){
+  if(worldData[row][(((col + 1) % ydim) + ydim) % ydim].type == water){
+    emptyNeighbours.push_back(std::make_pair(row, ((((col + 1) % ydim) + ydim) % ydim)));
+  }
+  if(worldData[row][(((col - 1) % ydim) + ydim) % ydim].type == water){
+    emptyNeighbours.push_back(std::make_pair(row, ((((col - 1) % ydim) + ydim) % ydim)));
+  }
+  if(worldData[(((row + 1) % xdim) + xdim) % xdim][col].type == water){
+    emptyNeighbours.push_back(std::make_pair(((((row + 1) % xdim) + xdim) % xdim), col));
+  }
+  if(worldData[(((row - 1) % xdim) + xdim) % xdim][col].type == water){
+    emptyNeighbours.push_back(std::make_pair(((((row - 1) % xdim) + xdim) % xdim), col));
+  }
+}
 
-// Move fish to random adjacent tile
-void fishMove(int row, int col){
-  if (!alreadyMoved){
+void fishUpdate(int row, int col){
+  std::vector<std::pair<int, int>> emptyNeighbours;
+  getEmptyNeighbours(row, col, emptyNeighbours);
+  worldData[row][col].breedTime++;
+  if(emptyNeighbours.size() > 0){
+    Tile moveTile = worldData[row][col];
+    int randChoice = rand() % emptyNeighbours.size();
+    std::pair<int, int> chosenTile = emptyNeighbours[randChoice];
+    worldData[chosenTile.first][chosenTile.second] = moveTile;
+    worldData[chosenTile.first][chosenTile.second].alreadyMoved = true;
+    if(worldData[row][col].breedTime >= worldData[row][col].breedOccur){
+      worldData[row][col].type = fish;
+      worldData[row][col].creatureColour = sf::Color::Green;
+      worldData[row][col].breedTime = 0;
+    }
+    else{
+      worldData[row][col].type = water;
+      worldData[row][col].creatureColour = sf::Color::Blue;
+    }
+  }
+}
+
+void sharkUpdate(int row, int col){
+  if(worldData[row][col].starveTime == worldData[row][col].starveDeath){
+    worldData[row][col].type = water;
+    worldData[row][col].breedTime = 0;
+    worldData[row][col].starveTime = 0;
+    worldData[row][col].creatureColour = sf::Color::Blue;
+  }
+  else {
+    std::vector<std::pair<int, int>> fishNeighbours;
+    getFishNeighbours(row, col, fishNeighbours);
     std::vector<std::pair<int, int>> emptyNeighbours;
     getEmptyNeighbours(row, col, emptyNeighbours);
-    if(emptyNeighbours.size() > 0){
+    worldData[row][col].breedTime++;
+    worldData[row][col].starveTime++;
+    if(fishNeighbours.size() > 0){
+      Tile moveTile = worldData[row][col];
+      int randChoice = rand() % fishNeighbours.size();
+      std::pair<int, int> chosenTile = fishNeighbours[randChoice];
+      worldData[chosenTile.first][chosenTile.second] = moveTile;
+      worldData[chosenTile.first][chosenTile.second].starveTime = 0;
+      worldData[chosenTile.first][chosenTile.second].alreadyMoved = true;
+      if(worldData[chosenTile.first][chosenTile.second].breedTime >= worldData[chosenTile.first][chosenTile.second].breedOccur){
+        worldData[row][col].type = shark;
+        worldData[row][col].creatureColour = sf::Color::Red;
+        worldData[chosenTile.first][chosenTile.second].breedTime = 0;
+      }
+      else{
+        worldData[row][col].type = water;
+        worldData[row][col].creatureColour = sf::Color::Blue;
+      }
+    }
+    else if(emptyNeighbours.size() > 0){
+      Tile moveTile = worldData[row][col];
       int randChoice = rand() % emptyNeighbours.size();
       std::pair<int, int> chosenTile = emptyNeighbours[randChoice];
-      worldData[row][col] = water;
-      std::cout << row << ", " << col << std::endl;
-      worldData[chosenTile.first][chosenTile.second] = fish;
-      std::cout << chosenTile.first << ", " << chosenTile.second << std::endl;
+      worldData[chosenTile.first][chosenTile.second] = moveTile;
+      worldData[chosenTile.first][chosenTile.second].breedTime++;
+      worldData[chosenTile.first][chosenTile.second].alreadyMoved = true;
+      if(worldData[row][col].breedTime >= worldData[row][col].breedOccur){
+        worldData[row][col].type = shark;
+        worldData[row][col].creatureColour = sf::Color::Red;
+        worldData[chosenTile.first][chosenTile.second].breedTime = 0;      
+      }
+      else{
+        worldData[row][col].type = water;
+        worldData[row][col].creatureColour = sf::Color::Blue;
+        worldData[chosenTile.first][chosenTile.second].breedTime++;
+      }
+    }
+  }
+}
+
+// Move creature to random adjacent tile
+void move(int row, int col){
+  if (!worldData[row][col].alreadyMoved){
+    if(worldData[row][col].type == fish){
+      fishUpdate(row, col);
+    }
+    else if(worldData[row][col].type == shark) {
+      sharkUpdate(row, col);
     }
   }
 }
 
 int main(){
+  //std::cout << ""
   int gridTotal = 0;
-  int fishPop = 20;
-  int sharkPop = 5;
+  int fishPop = 100;
+  int sharkPop = 20;
 
-  // Populate grid with fish, sharks and water tiles
+  // Populate grid with fish, shark and water tiles
   for(int i = 0; i < xdim; ++i){
     for(int k = 0; k < ydim; ++k){
       if(gridTotal < fishPop){
-        worldData[i][k] = new Fish();
+        worldData[i][k].type = fish;
+        worldData[i][k].breedOccur = 3;
+        worldData[i][k].creatureColour = sf::Color::Green;
       }
       else if(gridTotal < fishPop+sharkPop){
-        worldData[i][k] = new Shark();
+        worldData[i][k].type = shark;
+        worldData[i][k].breedOccur = 6;
+        worldData[i][k].starveDeath = 5;
+        worldData[i][k].creatureColour = sf::Color::Red;
       }
       else {
-        worldData[i][k] = new Water();
+        worldData[i][k].type = water;
+        worldData[i][k].creatureColour = sf::Color::Blue;
       }
       gridTotal++;
     }
@@ -135,7 +230,7 @@ int main(){
 
   // Flatten the 2D array and shuffle all of the tiles to randomly place fish and sharks
   srand (time(NULL)); 
-  GridTile* *oneDimArray = &worldData[0][0];
+  Tile *oneDimArray = &worldData[0][0];
   shuffle(oneDimArray, xdim, ydim);
 
   // Set colours of tiles to the appropriate colour based on whether it is a fish, shark or water
@@ -143,9 +238,7 @@ int main(){
     for(int k = 0; k < ydim; ++k){
       watorArray[i][k].setSize(sf::Vector2f(cellXSize,cellYSize));
       watorArray[i][k].setPosition(i*cellXSize,k*cellYSize);
-      if (worldData[i][k].getType() == water) watorArray[i][k].setFillColor(sf::Color::Blue);
-      else if (worldData[i][k].getType() == fish) watorArray[i][k].setFillColor(sf::Color::Green);
-      else if (worldData[i][k].getType() == shark) watorArray[i][k].setFillColor(sf::Color::Red);
+      watorArray[i][k].setFillColor(worldData[i][k].creatureColour);
     }
   }
 
@@ -161,30 +254,30 @@ int main(){
             window.close();
     }
 
-    float chronon = 0.5f;
+    float chronon = 0.1f;
     if(clock.getElapsedTime().asSeconds() >= chronon) {
       for(int i = 0; i < xdim; ++i){
         for(int k = 0; k < ydim; ++k){
-          if(worldData[i][k].getType() == fish){
-            fishMove(i, k);
+          if(worldData[i][k].type != water){
+            move(i, k);
           }
         }
       }
       clock.restart();
-    }  
+    }
+
+    moveReset();  
 
     // Set colours of tiles to the appropriate colour based on whether it is a fish, shark or water
     for(int i = 0; i < xdim; ++i){
       for(int k = 0; k < ydim; ++k){
-        if (worldData[i][k].getType() == water) watorArray[i][k].setFillColor(sf::Color::Blue);
-        else if (worldData[i][k].getType() == fish) watorArray[i][k].setFillColor(sf::Color::Green);
-        else if (worldData[i][k].getType() == shark) watorArray[i][k].setFillColor(sf::Color::Red);
+        watorArray[i][k].setFillColor(worldData[i][k].creatureColour);
       }
     }
 
     window.clear(sf::Color::Black);
     for(int i = 0; i < xdim; ++i){
-        for(int k = 0; k<ydim; ++k){
+        for(int k = 0; k < ydim; ++k){
             window.draw(watorArray[i][k]);
         }
     }
